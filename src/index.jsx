6955661011f5b731d75327506dd037b6ea2a7132
Willem Rosenthal <willemrosenthal@gmail.com>
2023-01-09 15:32:29 -0500

    removed solid rewind

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,11 @@ import { Router } from '@solidjs/router'
 
 import './index.css';
 import App from './App';
-import Rewind from 'solid-rewind';
 
 render(() => (
-  <Rewind>
     <Router>
       <App />
     </Router>
-  </Rewind>
   ), 
   document.getElementById('root')
 );
