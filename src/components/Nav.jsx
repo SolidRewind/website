878fc0543d3105cf9195f7de0731743ee09a84db
Willem Rosenthal <willemrosenthal@gmail.com>
2023-01-10 13:03:04 -0500

    fixed scroll

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,10 +6,12 @@ function Nav(props) {
     let diff = (element.offsetTop - window.scrollY);
     let scrollTo = start + diff * 0.05;
     window.scroll(0, scrollTo);
-    // var limit = window.height - window.innerHeight;
-    // console.log(window.outerHeight, window.screen.height);
-    if (diff > 10) setTimeout(scrollToId, 0 ,target, scrollTo);
-    // element && element.scrollIntoView({ behavior: "smooth", block: "start" });
+
+    let body = document.body, html = document.documentElement;
+    let height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+    let limit = height - window.innerHeight;
+    
+    if (((diff > 1 && Math.sign(diff) > 0) || (diff < 1 && Math.sign(diff) < 0)) && scrollTo < limit) setTimeout(scrollToId, 0 ,target, scrollTo);
   }
   
 
@@ -24,10 +26,10 @@ function Nav(props) {
     <nav class={(props.mode==='docs' && 'docsTopNav')}>
       <nav class='nav'>
         {/* href='/#install' */}
-        { (props.mode!=='docs') && <div class='navItem leftNavItem'><a onclick={()=>scrollToId('install')} >Get Started</a></div> }
+        { (props.mode!=='docs') && <div class='navItem leftNavItem'><a onclick={()=>scrollToId('install', window.scrollY)} >Get Started</a></div> }
         { (props.mode==='docs') && <div class='navItem leftNavItem'><a href="/" >Solid Rewind</a></div> }
         <div class='navSpacer'></div>
-        <div class='navItem'><a onclick={()=>scrollToId('team')} >Team</a></div>
+        <div class='navItem'><a onclick={()=>scrollToId('team', window.scrollY)} >Team</a></div>
         <div class='navItem'><a href='/demo'>Demo</a></div>
         {/* <div class='navItem'><a href='/docs/installation'>Docs</a></div> */}
         <div class='navItem'><a href='https://github.com/oslabs-beta/solid-rewind' target='_blank'>Github</a></div>
