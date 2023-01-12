ce859c2c80aab132da35c3e7fba02ee2d077e9d8
Willem Rosenthal <willemrosenthal@gmail.com>
2023-01-12 15:54:30 -0500

    removed duplicate inport

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import solidPlugin from "vite-plugin-solid";
-
-import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
