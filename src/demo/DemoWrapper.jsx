ae523437fceefa51c11a721c89795a6bb5b2b410
Willem Rosenthal <willemrosenthal@gmail.com>
2023-01-12 12:55:32 -0500

    solid rw put back at top level

--- src/demo/DemoWrapper.jsx
+++ src/demo/DemoWrapper.jsx
@@ -1,15 +1,17 @@
-import Rewind from "solid-rewind";
+// import Rewind from "solid-rewind";
 // import Rewind from '../../../solid-rewind/rewind-devtool/src/Rewind';
 import Demo from "./Demo.jsx";
 function DemoWrapper() {
 
   return (
-    <Rewind runInProduction={true}>
+    // <Rewind runInProduction={true}>
+    <div>
       <div class="hideOverflow">
         <Demo/>
       </div>
       <iframe src = "./IframeDemo" id='IframeDemo' class='timelineContainer'></iframe>
-    </Rewind>
+    {/* </Rewind> */}
+    </div>
   );
 }
 
