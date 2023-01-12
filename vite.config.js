4119a93cf17db524965aca360cb6f9e7717e059d
Willem Rosenthal <willemrosenthal@gmail.com>
2023-01-12 15:58:07 -0500

    defined mode 3

--- vite.config.js
+++ vite.config.js
@@ -9,20 +9,21 @@ export default defineConfig(({ command, mode }) => {
     // vite config
     define: {
       __APP_ENV__: env.APP_ENV,
-      plugins: [solidPlugin()],
-      server: {
-        port: 3333,
-      },
-      build: {
-        target: "esnext",
-      },
-      optimizeDeps: {
-        // Add both @codemirror/state and @codemirror/view to included deps to optimize
-        include: ["@codemirror/state", "@codemirror/view"],
-      },
-      mode: 'development'
-    }
+    },
+    plugins: [solidPlugin()],
+    server: {
+      port: 3333,
+    },
+    build: {
+      target: "esnext",
+    },
+    optimizeDeps: {
+      // Add both @codemirror/state and @codemirror/view to included deps to optimize
+      include: ["@codemirror/state", "@codemirror/view"],
+    },
+    mode: 'development'
   }
+  
 })
 
 // export default defineConfig({
