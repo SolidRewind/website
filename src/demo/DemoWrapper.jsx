b2f3f73d74e285722740a13cf0c09a7bc23b0a6b
Willem Rosenthal <willemrosenthal@gmail.com>
2023-01-12 10:58:13 -0500

    made all demo paths lowercase

--- src/demo/DemoWrapper.jsx
+++ src/demo/DemoWrapper.jsx
@@ -1,6 +1,6 @@
 import Rewind from "solid-rewind";
 // import Rewind from '../../../solid-rewind/rewind-devtool/src/Rewind';
-import Demo from "./Demo";
+import Demo from "./Demo.jsx";
 function DemoWrapper() {
 
   return (
