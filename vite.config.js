5ecbeb1cf7a140894da31247e17a00829ae2cd2a
Willem Rosenthal <willemrosenthal@gmail.com>
2023-01-12 13:45:25 -0500

    removed vite aliases

--- vite.config.js
+++ vite.config.js
@@ -13,11 +13,11 @@ export default defineConfig({
     // Add both @codemirror/state and @codemirror/view to included deps to optimize
     include: ["@codemirror/state", "@codemirror/view"],
   },
-  resolve: {
-    alias: {
-      'solid-js' : 'solid-js/dist/dev.js',
-      'solid-js/web' : 'solid-js/web/dist/dev.js',
-      'solid-js/store' : 'solid-js/store/dist/dev.js',
-    }
-  }
+  // resolve: {
+  //   alias: {
+  //     'solid-js' : 'solid-js/dist/dev.js',
+  //     'solid-js/web' : 'solid-js/web/dist/dev.js',
+  //     'solid-js/store' : 'solid-js/store/dist/dev.js',
+  //   }
+  // }
 });
